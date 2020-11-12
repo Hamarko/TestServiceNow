756e9b04bdd72e48da425014055a3396bea1f6ef
Hamarko <hamarkoav@gmail.com>
2020-11-12 20:04:43 +0200

    4

--- TestIncidentList/src/x-552910-incident-list/index.js
+++ TestIncidentList/src/x-552910-incident-list/index.js
@@ -4,53 +4,63 @@ const {COMPONENT_BOOTSTRAPPED} = actionTypes;
 import snabbdom from '@servicenow/ui-renderer-snabbdom';
 import "@servicenow/now-template-card";
 import styles from './styles.scss';
-const view = (state, {updateState}) => {
-	console.log(state)
-	const {short_description, number, state_, assignment_group, assigned_to,updated_on} = state;	
-	console.log(state)
-	return (		
-		<div className="wrapper">
-			<h1>Card</h1>
-			{data.map( d => (
-				<now-template-card-assist 
-				tagline={{"icon":"tree-view-long-outline","label":"Process"}}
-				actions={[{"id":"share","label":"Copy URL"},
-						  {"id":"close","label":"Mark Complete"}]}
-				heading={short_description}
-				content={[number, state_, assignment_group, assigned_to]}
-				contentItemMinWidth="300"
-				footerContent={updated_on}
-				configAria={{}}/>
-			))}				 		
-		</div>
-	);
+const FechData = createHttpEffect('api/now/table/incident', {
+	method: 'GET',	
+	queryParams: ['sysparm_display_value'],
+	successActionType: 'FETCH_LATEST_INCIDENT_SUCCESS'
+})
+console.log(FechData)
+const view = (state, {updateState}) => {	
+	const {incidents} = state;		
+    if(incidents){
+		return (		
+			<div className="wrapper">
+				<h1>Incident</h1>
+				{incidents.map( d => (				
+					<now-template-card-assist 
+					tagline={{"icon":"tree-view-long-outline","label":"Incident"}}
+					actions={[{"id":"share","label":"Copy URL"},
+							{"id":"close","label":"Mark Complete"}]}
+					heading={{"label":d.short_description}}
+					content={[{"label":"Nuber","value":{"type":"string","value":d.number}},
+							{"label":"State","value":{"type":"string","value":d.state_}},						  
+							{"label":"Assignment group","value":{"type":"string","value":d.assignment_group.display_value}},
+							{"label":"Assigned to","value":{"type":"string","value": d.assigned_to.display_value}}						  
+							]}
+					contentItemMinWidth="400"
+					footerContent={{"label":"Updated","value":d.sys_updated_on}}
+					configAria={{}}/>
+				))}				 		
+			</div>
+		);
+	}else{return (<div className="wrapper">	<h1>Incident</h1></div>)}
+
 };
 
-createCustomElement('x-552910-incident-list', {
-	renderer: {type: snabbdom},
-	view,
+createCustomElement('x-552910-incident-list', {	
 	actionHandlers: {
 		[COMPONENT_BOOTSTRAPPED]: (coeffects) => {
-			const { dispatch } = coeffects;
-		
+			const { dispatch } = coeffects;		
 			dispatch('FETCH_LATEST_INCIDENT', {
-				sysparm_limit: '1',
-				sysparm_query: 'ORDERBYDESCnumber'
+				sysparm_display_value:'true'
 			});
 		},
-		'FETCH_LATEST_INCIDENT': createHttpEffect('api/now/table/incident', {
-			method: 'GET',
-			queryParams: ['sysparm_limit','sysparm_query'],
-			successActionType: 'FETCH_LATEST_INCIDENT_SUCCESS'
-		}),
-		'FETCH_LATEST_INCIDENT_SUCCESS': (coeffects) => {
+		'FETCH_LATEST_INCIDENT': FechData,
+		'FETCH_LATEST_INCIDENT_SUCCESS': (coeffects) => {			
 			const { action, updateState } = coeffects;
-			const { result } = action.payload;
-			const {  short_description, number, state_, assignment_group, assigned_to,updated_on  } = result[0];
-					
-			updateState({ short_description, number, state_, assignment_group, assigned_to,updated_on });
+			const { result } = action.payload;			
+			const incidents = []
+			console.log(result)
+			result.map(incident => {				
+				let {  short_description, number,assignment_group, assigned_to,sys_updated_on  } = incident
+				let state_ = incident.state
+				incidents.push({ short_description, number, state_, assignment_group, assigned_to,sys_updated_on })
+			})					
+			updateState({incidents});
 		}
 
-	},	
+	},
+	renderer: {type: snabbdom},
+	view,	
 	styles
 });
