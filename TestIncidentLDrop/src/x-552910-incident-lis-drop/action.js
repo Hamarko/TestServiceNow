0ba9f8b321f8a2e7a7a9064cab51191f81f44614
Hamarko <hamarkoav@gmail.com>
2020-11-13 20:23:08 +0200

    5_1

--- TestIncidentLDrop/src/x-552910-incident-lis-drop/action.js
+++ TestIncidentLDrop/src/x-552910-incident-lis-drop/action.js
@@ -1,13 +1,6 @@
 import { actionTypes} from '@servicenow/ui-core';
 import { createHttpEffect } from '@servicenow/ui-effect-http';
 const {COMPONENT_BOOTSTRAPPED} = actionTypes;
-const fech = createHttpEffect('api/now/table/incident', {
-	method: 'GET',	
-	queryParams: ['sysparm_display_value'],
-	successActionType: 'FETCH_INCIDENT_SUCCESS',
-	errorActionType:"ERROR"
-})
-console.log(fech)
 export default {
     actionHandlers: {
         [COMPONENT_BOOTSTRAPPED]: (coeffects) => {
@@ -17,15 +10,48 @@ export default {
 				sysparm_display_value:'true'
 			});
 		},
-		'NOW_DROPDOWN_PANEL#ITEM_CLICKED':(v) =>{
-			console.log(v)},
-        'FETCH_INCIDENT': fech,
+		'NOW_DROPDOWN_PANEL#ITEM_CLICKED':	(coeffects) =>{
+			const { action, updateState, dispatch, state} = coeffects;
+			console.log(action.payload)				
+			const id = action.payload.item.id.split(' ')[0];
+			const button = action.payload.item.id.split(' ')[1];
+			console.log(button)			
+			if(button === "Open_Record"){
+				const opened = true;
+			    updateState({id, opened});
+			} else if(button === "Delete") {
+				console.log(button,id)				
+				updateState({
+					result: state.result.filter(incident=>incident.sys_id !== id)
+				});
+				dispatch('DELETED_INCIDENT', {id})
+			}			
+		},
+		'NOW_MODAL#OPENED_SET': (coeffects) => {
+			const { action, updateState } = coeffects;
+			const opened = action.payload.value
+			updateState({opened})
+		},
+        'FETCH_INCIDENT': createHttpEffect('api/now/table/incident', {
+			method: 'GET',	
+			queryParams: ['sysparm_display_value'],
+			successActionType: 'FETCH_INCIDENT_SUCCESS',
+			errorActionType:"ERROR"
+		}),
         'FETCH_INCIDENT_SUCCESS': (coeffects) => {						
 			const { action, updateState } = coeffects;			
 			const { result } = action.payload;					
 			updateState({result});
 		},
-		'ERROR': (error) => console.log(error)
+		'ERROR': (error) => (coeffects) => {
+			const { action } = coeffects
+			console.log( action, coeffects )
+		},
+		'DELETED_INCIDENT': createHttpEffect('api/now/table/incident/:',{
+			method: 'DELETE',
+			pathParams: ['id'],
+			errorActionType:"ERROR"
+		})
 
     }
 
--- TestIncidentLDrop/src/x-552910-incident-lis-drop/view.js
+++ TestIncidentLDrop/src/x-552910-incident-lis-drop/view.js
@@ -1,17 +1,31 @@
 import "@servicenow/now-template-card";
-
+import "@servicenow/now-modal";
+import "@servicenow/now-input";
 export default (state, {updateState}) => {
-    const {result} = state;
-    console.log(result)
+    const {result,opened = 'false',id} = state;
+    const modalContent = [['','']]      
+    if(opened === true){
+       console.log(result, id)
+       const card = result.find( e => (e.sys_id === id)? true : false);
+       const modalInstance = [["Incident",card.short_description],
+                              ["Nuber",card.number], 
+                              ["State",card.state],
+                              ["Assignment group", card.assignment_group.display_value],
+                              ["Assigned to","value",card.assigned_to.display_value]];
+       modalContent.pop();
+       modalInstance.forEach(e => modalContent.push(e));  
+       console.log(modalContent) 
+       
+    } 
     if (result) {
-        return (
-            <div className="wrapper">
-				<h1>Incident</h1>
+        return (                                           
+            <div className="wrapper">                                                          
+				<h1>Incident</h1>                                                                         
                 {result.map( d => (				
                     <now-template-card-assist 
                         tagline={{"icon":"tree-view-long-outline","label":"Incident"}}
-                        actions={[{"id":"share","label":"Open Record"},
-                                {"id":"close","label":"Delete"}]}
+                        actions={[{"id":d.sys_id+" Open_Record","label":"Open Record"},
+                                {"id":d.sys_id+" Delete","label":"Delete"}]}
                         heading={{"label":d.short_description}}
                         content={[{"label":"Nuber","value":{"type":"string","value":d.number}},
                                 {"label":"State","value":{"type":"string","value":d.state}},						  
@@ -21,7 +35,17 @@ export default (state, {updateState}) => {
                         contentItemMinWidth="400"
                         footerContent={{"label":"Updated","value":d.sys_updated_on}}
                         configAria={{}}/>
-                ))}				 		
+                ))}
+                <now-modal 
+                     size="lg" headerLabel="Modal header"
+                     opened = {opened}                    
+                     footerActions={[{"label":"Delete","variant":"primary-negative"}]}>
+                        <div className="now-input">
+                            {modalContent.map(inputProps => 
+                            (<now-input label={inputProps[0]}  
+                            step="any" type="text" value={inputProps[1]} />))}                            
+                        </div>                                                        
+                 </now-modal>               				 		
 			</div>		
         );
     }
